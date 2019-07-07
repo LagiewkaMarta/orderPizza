68cc937c0c72d9844aaf4f57df92365b442bda2f
Marta <lagiewka.marta.dev@gmail.com>
2019-07-07 09:17:35 +0200

    Add Google fonts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Open Sans', sans-serif;
+    background-color: red;
+  }
+  h1, h2, h3 {
+    font-family: 'Righteous', cursive;
+  }
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <>
+      <GlobalStyle />
+      <h1>Sliceline</h1>
+      <div>hello</div>
+    </>
   );
 }
 
