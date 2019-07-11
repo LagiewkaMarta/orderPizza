bbc30597378fa9df7fdb05e155e51d232e5e00b9
Marta <lagiewka.marta.dev@gmail.com>
2019-07-12 00:04:55 +0200

    List Orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,35 +2,60 @@ import React from "react";
 import styled from "styled-components";
 import Button from "../Reusables/Button";
 
-const Order = ({className, orders}) => {
-    return (
-        <section className={className}>
-{orders.length}
-            <Button text="order now"/>
-        </section>
-    )
-}
+const OrderList = styled.ul`
+  list-style: none;
+  width: 100%;
+`;
+const OrderItem = styled.li`
+  border: 2px solid gray;
+  width: 100%;
+  padding: 1rem;
+  margin-top: .3rem;
+`;
+const Order = ({ className, orders }) => {
+  return (
+    <section className={className}>
+      {orders.length === 0 ? (
+        <p>Your order seems pretty empty</p>
+      ) : (
+        <OrderList>
+            Items to be ordered:
+            {orders.map(order => {
+                return (
+                    <OrderItem>{order.name}</OrderItem>
+                )
+            })}
+        </OrderList>
+      )}
+      <Button text="order now" />
+    </section>
+  );
+};
 
 export default styled(Order)`
-position: fixed;
-right: 0;
-top: 4.9rem;
-height: 100vh;
-width: 32vw;
-background-color: #fff;
-box-shadow: 2px 2px 20px 2px gray;
-display: flex;
-flex-direction: column;
-background-color: orange;
-justify-content: space-between;
-align-items: center;
-p{
+  position: fixed;
+  right: 0;
+  top: 4.9rem;
+  height: 100vh;
+  width: 32vw;
+  background-color: #fff;
+  box-shadow: 2px 2px 20px 2px gray;
+  display: flex;
+  flex-direction: column;
+  background-color: orange;
+  justify-content: space-between;
+  align-items: center;
+  p {
     min-height: 30vh;
-    background-color: blue;
+    /* background-color: blue; */
     width: 100%;
-}
-button {
+  }
+  button {
     margin-bottom: 6vh;
     width: 60%;
-}
-`
+  }
+  ul {
+      max-height: 70vh;
+      overflow: auto;
+  }
+`;
